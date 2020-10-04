17c81a80179b9573ccb6eb7bb51b83e7104fede6
dannys0n <72278864+dannys0n@users.noreply.github.com>
2020-10-03 21:30:49 -0700

    Add files via upload

---
+++ bubbles.js
@@ -0,0 +1,13 @@
+document.querySelector("#instagram")
+.addEventListener("click", () => {
+    window.location.href = "https://www.instagram.com/daihan__son/?hl=en";
+});
+
+document.querySelector("#facebook")
+.addEventListener("click", () => {
+    window.location.href = "https://www.facebook.com/danny.son.750331";
+});
+document.querySelector("#info")
+.addEventListener("click", () => {
+    window.location.href = "https://www.google.com/";
+});
